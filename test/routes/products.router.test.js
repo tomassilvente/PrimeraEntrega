82f1dc63d04e39448129b6e863a62f938e57f8ee
Tomas <silventetomas@gmail.com>
2023-10-13 10:39:08 -0300

    updating

--- test/routes/products.router.test.js
+++ test/routes/products.router.test.js
@@ -24,7 +24,7 @@ describe("Carts Router test Case", ()=>{
         expect(response.statusCode).to.be.eql(200) 
         expect(response.body.payload._id).to.be.ok
         expect(response.body.payload.title).to.be.eql("Producto de prueba")
-        await dropProducts()
+        expect(response.body.payload.stock).to.be.eql(999)
     })
     it('[POST] expect a 400 because of wrong data', async () =>{
         const mockProducts={ 
@@ -38,7 +38,7 @@ describe("Carts Router test Case", ()=>{
         }
         const response = await requester.post('/api/products').send(mockProducts)
         expect(response.statusCode).to.be.eql(400) 
-        await dropProducts()
+        
     })
     it('[GET] api/products - todos los productos correctamente', async function(){
         const response = await requester.get('/api/products')
